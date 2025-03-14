abb1ab2ce5dab383e5dc20d4e7f0584169807581
Andrey <ustavitskii@sfedu.ru>
2025-03-14 19:33:59 +0300

    Initial commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,9 @@
+// src/app/components/Header.js
+const Header = () => (
+    <header className="bg-blue-500 text-white p-4">
+      <h1>Мой первый проект на Next.js</h1>
+    </header>
+  );
+  
+  export default Header;
+  
