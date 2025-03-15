dc8194c2d224ec096634c36c61769b96eb87da3d
Andreyhai <ustavitski.andrei@gmail.com>
2025-03-15 10:03:32 +0300

    test commit-autodeply

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,7 +1,7 @@
 // src/app/components/Header.js
 const Header = () => (
     <header className="bg-blue-500 text-white p-4">
-      <h1>Мой первый проект на Next.js</h1>
+      <h1>Мой первый проект на Next.js version 2</h1>
     </header>
   );
   
