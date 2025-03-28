3bc9b18dcac3ba4cabc697e6ccb9d39a727e8c60
Andreyhai <ustavitski.andrei@gmail.com>
2025-03-28 21:46:56 +0300

    home-page

--- src/app/components/sections/hero/HeroSection.js
+++ src/app/components/sections/hero/HeroSection.js
@@ -20,7 +20,7 @@ const HeroSection = () => {
                 transition={{ duration: 1, ease: 'easeOut', delay: 2 }}
                 className={styles.container}
             >
-                <h1>Let's Create Your Dream Interior</h1>
+                <h1>{'Let\'s Create Your Dream Interior'}</h1>
                 <p>The world needs innovators and problem solvers who turn challenges into greater opportunities.</p>
                 <DarkButton title="Get Started" src={"/icons/arrows/arrowRight.svg"} color="black" />
             </motion.div>
